0ec12941beb40e7a10bdf52627904d1e47da34e4
Amritanshu Ranjan <84428101+amrit2000-super@users.noreply.github.com>
2022-08-09 19:56:44 +0530

    ALP to find no of times a pattern is repeated in a given text

--- repition.asm
+++ repition.asm
@@ -1,25 +1,37 @@
-AREA PROG21,CODE,READONLY
-CNT RN 7
-ENTRY
-      LDR R1,=M
-      LDR R2,=S
-      MOV R12,R2
-      MOV CNT,#0
-LOOP  LDRB R3,[R1]
-      LDRB R4,[R2]
-      CMP R4,#0
-      ADDEQ CNT,CNT,#1
-      MOVEQ R2,R12
-      BEQ LOOP
-      CMP R3,R4
-      ADDEQ R2,R2,#1
-      MOVNE R2,R12
-      ADD R1,R1,#1
-      BEQ LOOP
-      CMP R3,#0
-      BEQ STOP
-      BNE LOOP
-STOP B STOP
-M DCB "ABCABC",0
-S DCB "ABC",0
-END
+area proj15,code,readonly
+entry
+      mov r1,#0
+	  mov r2,#0
+	  mov r10,#0
+	  ldr r3,=pattern
+	  ldr r4,=text
+	  bl findlengthofpattern
+repeat  ldrb r0,[r3]
+        ldrb r6,[r4],#1
+		cmp r6,#0
+		beq stop
+		cmp r0,r6
+		bne repeat
+		mov r7,r2
+		sub r7,r7,#1
+		mov r8,r3
+		mov r9,r4
+find    ldrb r0,[r8,#1]!
+        ldrb r6,[r9],#1
+		cmp r0,r6
+		bne repeat
+		subs r7,r7,#1
+		bne find
+		add r10,r10,#1
+		b repeat
+stop    b stop
+findlengthofpattern  ldrb r5,[r3],#1
+					 cmp r5,#0
+					 addne r2,r2,#1
+					 bne findlengthofpattern
+					 sub r3,r3,r2
+					 sub r3,r3,#1
+					 bx lr
+pattern dcb "ABC",0
+text dcb "ABCABC",0
+end
