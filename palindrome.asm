3b4482f506a1d30d80bd252a05a6dfe91a7c1e77
Amritanshu Ranjan <84428101+amrit2000-super@users.noreply.github.com>
2022-08-09 16:40:00 +0530

    ALP to check whether a given number is palindrome or not

--- palindrome.asm
+++ palindrome.asm
@@ -1,12 +1,29 @@
-area proj17,code,readonly
+divisor rn 0
+dividend rn 1
+remainder rn 2
+result rn 3
+quotient rn 6
 entry
-      ldr r0,=number
-	  mov r1,#0x40000000
-	  mov r2,#0x40000000
-loop1 ldr r6,[r0],#4
-      str r6,[r1],#4
-      cmp r0,#0
-	  bne loop1
+       ldr r4,=121
+	   mov divisor,#10
+	   mov dividend,r4
+	   mov remainder,#0
+	   mov result,#0
+	   mov r5,r4
+loop   bl div
+       mla result,divisor,result,remainder
+       movs dividend,quotient
+	   mov quotient,#0
+	   mov remainder,#0
+	   cmp dividend,#0
+	   bne loop
+	   cmp r5,result
+	   moveq r7,#1
+	   movne r7,#0
 stop b stop
-number dcd 121
+div    subs dividend,dividend,divisor
+       addpl quotient,quotient,#1
+	   bpl div
+	   addmi remainder,dividend,divisor
+	   bx lr
 end
